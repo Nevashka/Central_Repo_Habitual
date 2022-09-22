ba1bacab0c91f4fb14893ac102fc4f5aa05c1f55
Matthew Lo <matthewlol316@gmail.com>
2022-09-22 23:37:56 +0100

    add heroku link, netlify

--- db/migrations/1_create_habits.sql
+++ db/migrations/1_create_habits.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS habits;
 CREATE TABLE habits(
     id serial PRIMARY KEY,
     habit_name varchar(255) NOT NULL UNIQUE,
-    date_complete integer[],
+    date_complete DATE,
     period int NOT NULL,
     frequency int NOT NULL,
     frequency_done integer NOT NULL,
